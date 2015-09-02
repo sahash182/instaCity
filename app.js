5b874c64813471cc4f3dc0da70ef4c82c695f331
Sahash <sahash182@gmail.com>
2015-09-01 18:27:48 -0700

    styling

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ angular.module('instaCity', ['ngRoute'])
       .when('/', {
         templateUrl: 'templates/search.html',
         controller: 'MainCtrl'
+      });
+    $routeProvider
+      .when('/about',{
+        templateUrl: 'templates/about.html',
       })
 
     $locationProvider.html5Mode({
